5ef596ead4eaa29aa3ec9d7ba41ea03fcb1638bb
VoidZephyr <jking21412@gmail.com>
2024-11-05 07:58:59 -0800

    1st

---
+++ SDI-Project-2/src/Features/Episodes/episodes.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { fetchEpisodes} from '../../api';
+
+
+function episodeList() {
+    const [episodes, setEpisodes] = useState([]);
+
+    useEffect(() => {
+        const getEpisodes = asyc () = > {
+            const data = await fetchEpisodes();
+            setEpisodes(data);
+        };
+        getEpisodes();
+    }, []);
+
+    return (
+        <div>
+        <h1>Episodes</h1>
+        <div>
+            {episodes.map((episode) => (
+            <div key={episode.id}>
+                <img src={episode.image} alt={episode.name} />
+                <h2>{episode.name}</h2>
+                <p>{episode.species}</p>
+            </div>
+            ))}
+        </div>
+        </div>
+    );
+    }
+    
+export default episodeList; 
