abfca4336c4383ef5119cebcebb77885c6c079b1
VoidZephyr <jking21412@gmail.com>
2024-11-05 07:40:56 -0800

    1st

---
+++ SDI-Project-2/src/Features/Characters/characters.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { fetchCharacters } from '../../api';
+
+
+function CharacterList() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const getCharacters = asyc () = > {
+            const data = await fetchCharacters();
+            setCharacters(data);
+        };
+        getCharacters();
+    }, []);
+
+    return (
+        <div>
+        <h1>Characters</h1>
+        <div>
+            {characters.map((character) => (
+            <div key={character.id}>
+                <img src={character.image} alt={character.name} />
+                <h2>{character.name}</h2>
+                <p>{character.species}</p>
+            </div>
+            ))}
+        </div>
+        </div>
+    );
+    }
+    
+export default CharacterList; 
